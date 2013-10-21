2212fd80df36f0ca6c17af94a5343a244c1a5d76
Matt Ott <mott@he.net>
2013-10-21 01:05:33 -0700

    More error handling

--- GestureCalculator/src/main/java/com/ottmatt/gesturecalculator/MainActivity.java
+++ GestureCalculator/src/main/java/com/ottmatt/gesturecalculator/MainActivity.java
@@ -1,15 +1,21 @@
 package com.ottmatt.gesturecalculator;
 
+import android.content.Context;
 import android.content.res.TypedArray;
+import android.hardware.display.DisplayManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.hardware.display.DisplayManagerCompat;
 import android.support.v4.view.GestureDetectorCompat;
 import android.util.Log;
+import android.view.Display;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -26,6 +32,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
     private static final int MULT_DIV = 3;
     private static final int ADD_SUB = 4;
     private static int TOUCH_SLOP;
+    private static int DISPLAY_WIDTH;
     private GestureDetectorCompat mDetector;
     private View.OnTouchListener mDigitListener;
     private GestureDetectorCompat mViewerDetector;
@@ -33,6 +40,12 @@ public class MainActivity extends Activity implements View.OnClickListener{
     private TextView mTextViewer;
     private List<Hashtable<String, Integer>> mEquation;
 
+    @Override public void onAttachedToWindow() {
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        //noinspection deprecation
+        DISPLAY_WIDTH = display.getWidth();
+    }
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -66,6 +79,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
             }
             button_digits.recycle();
         }
+
         mTextViewer = (TextView)findViewById(R.id.text_viewer);
         mTextViewer.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
@@ -87,20 +101,36 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
 
     void push(String value, int order) {
+        Log.v(TAG, "push()");
         Hashtable<String, Integer> math = new Hashtable<String, Integer>();
         math.put(value, order);
         // If the last value was an operator and this value is an operator
         // then replace the old operator with the new one
-//        if (!mEquation.get(mEquation.size()-1).containsValue(DIGIT) &&
-            //    order != DIGIT)
-           // mEquation.set(mEquation.size()-1, math);
-       // else
-            mEquation.add(math);
+        switch (order) {
+            case MULT_DIV:
+                if (!mEquation.isEmpty()) {
+                    if (!mEquation.get(mEquation.size()-1).containsValue(DIGIT))
+                        mEquation.set(mEquation.size()-1, math);
+                    else
+                        mEquation.add(math);
+                }
+                break;
+            case ADD_SUB:
+                if (!mEquation.isEmpty() && !mEquation.get(mEquation.size()-1).containsValue(DIGIT)) {
+                    mEquation.set(mEquation.size()-1, math);
+                    break;
+                }
+            default:
+                mEquation.add(math);
+        }
+        refreshViewer();
     }
 
     void pop() {
         if (!mEquation.isEmpty())
             mEquation.remove(mEquation.size() - 1);
+
+        refreshViewer();
     }
 
     String stackToString() {
@@ -122,7 +152,10 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
 
     void clearViewer() {
-        mEquation.clear();
+        if (!mEquation.isEmpty())
+            mEquation.clear();
+
+        refreshViewer();
     }
 
     public void onClick(View v) {
@@ -134,11 +167,16 @@ public class MainActivity extends Activity implements View.OnClickListener{
         }
 
         push(input, DIGIT);
-        refreshViewer();
     }
 
     class GestureListener extends GestureDetector.SimpleOnGestureListener {
         private static final String TAG = "Gestures";
+        private boolean operatorEligible = false;
+
+        @Override public boolean onDown(MotionEvent e) {
+            operatorEligible = true;
+            return false;
+        }
 
         @Override public boolean onScroll(MotionEvent e1, MotionEvent e2,
                                          float distanceX, float distanceY) {
@@ -146,27 +184,32 @@ public class MainActivity extends Activity implements View.OnClickListener{
             float deltaY = e1.getY() - e2.getY();
 
             try {
-                Log.d(TAG, "onScroll: " + e1.toString()+e2.toString());
-                if (Math.abs(deltaY) <= SWIPE_MAX_OFF_PATH) {
-                    if(deltaX > TOUCH_SLOP) {
-                        // scroll left
-                        push("-", ADD_SUB);
-                    } else if (deltaX < -TOUCH_SLOP) {
-                        // scroll right
-                        push("+", ADD_SUB);
-                    }
-                } else {
-                    if(deltaY > TOUCH_SLOP) {
-                        // scroll up
-                        push("*", MULT_DIV);
-                    } else if (deltaY < -TOUCH_SLOP) {
-                        // scroll down
-                        push("/", MULT_DIV);
+                if (operatorEligible) {
+                    Log.d(TAG, "onScroll: " + e1.toString()+e2.toString());
+                    if (Math.abs(deltaY) <= SWIPE_MAX_OFF_PATH) {
+                        if(deltaX > TOUCH_SLOP) {
+                            // scroll left
+                            push("-", ADD_SUB);
+                            operatorEligible = false;
+                        } else if (deltaX < -TOUCH_SLOP) {
+                            // scroll right
+                            push("+", ADD_SUB);
+                            operatorEligible = false;
+                        }
+                    } else {
+                        if(deltaY > TOUCH_SLOP) {
+                            // scroll up
+                            push("*", MULT_DIV);
+                            operatorEligible = false;
+                        } else if (deltaY < -TOUCH_SLOP) {
+                            // scroll down
+                            push("/", MULT_DIV);
+                            operatorEligible = false;
+                        }
                     }
                 }
-                refreshViewer();
             } catch (Exception e) {
-                // nothing
+                Log.v(TAG, e.getMessage());
             }
             return false;
         }
@@ -176,14 +219,12 @@ public class MainActivity extends Activity implements View.OnClickListener{
         private static final String TAG = "ViewerGestures";
 
         @Override public boolean onSingleTapUp(MotionEvent e) {
-            int x = e.AXIS_X;
-            int y = e.AXIS_Y;
-            int viewWidth = mTextViewer.getWidth();
-            if (x < viewWidth/2)
+            float x = e.getRawX();
+
+            if (x < DISPLAY_WIDTH/2)
                 pop();
             else
                 push("=", DIGIT);
-            refreshViewer();
             return false;
         }
 
@@ -198,20 +239,19 @@ public class MainActivity extends Activity implements View.OnClickListener{
                     if (deltaX > TOUCH_SLOP) {
                         // scroll left
                         //pop();
-                    } else if (deltaX < TOUCH_SLOP) {
+                    } else if (deltaX < -TOUCH_SLOP) {
                         // scroll left
                     }
                 } else {
                     if(deltaY > TOUCH_SLOP) {
                         // scroll up
-                    } else if (deltaY < TOUCH_SLOP) {
+                    } else if (deltaY < -TOUCH_SLOP) {
                         // scroll down
                         clearViewer();
                     }
                 }
-                refreshViewer();
             } catch (Exception e) {
-                // nothing
+                Log.v(TAG, e.getMessage());
             }
             return false;
         }
